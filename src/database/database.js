cde54d14bfba994b9c46727274585116b77886b4
weliton <weliton.ww@hotmail.com>
2025-06-27 14:43:07 -0300

    api finish

--- src/database/database.js
+++ src/database/database.js
@@ -47,7 +47,7 @@ export class Database {
       const rowIndex = this.#database[table].findIndex((row) => 
       row.id === id
     )
-    if(rowIndex){
+    if(rowIndex  > -1){
       this.#database[table][rowIndex] = {
         ...this.#database[table][rowIndex],
         ...data
@@ -55,5 +55,17 @@ export class Database {
       
       this.#persist()
     }
+  }
+
+  delete(table, id){
+    const rowIndex = this.#database[table].findIndex((row) => 
+      row.id === id
+    )
+
+    if(rowIndex  > -1){
+      this.#database[table].splice(rowIndex, 1)
+      this.#persist()
+    }
+
   }
 }
--- src/routes/tickets.js
+++ src/routes/tickets.js
@@ -1,6 +1,8 @@
 import { create } from '../controllers/tickets/create.js'
 import { index } from '../controllers/tickets/index.js'
-import { update } from '../controllers/tickets/update.js'
+import { update } from '../controllers/tickets/update.js' 
+import { updateStatus } from '../controllers/tickets/updateStatus.js'
+import { remove } from '../controllers/tickets/remove.js'
 
 export const ticketsRoutes = [
   {
@@ -17,6 +19,16 @@ export const ticketsRoutes = [
     method: 'PUT',
     path: '/tickets/:id',
     controller: update
+  },
+  {
+    method: 'PATCH',
+    path: '/tickets/:id/close',
+    controller: updateStatus
+  },
+  {
+    method: 'DELETE',
+    path: '/tickets/:id',
+    controller: remove
   },
 
 ]
